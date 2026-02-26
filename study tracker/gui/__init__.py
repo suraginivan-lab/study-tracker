14f885e674e92342d1e1b68176fd9e34889d32da
suraginivan-lab <suraginivan@gmail.com>
2026-02-26 17:47:37 +0300

    Delete study tracker directory

--- study tracker/gui/__init__.py
+++
@@ -1,6 +0,0 @@
-from .main_window import MainWindow
-from .add_edit_dialog import AddEditDialog
-from .categories_dialog import CategoriesDialog
-from .tags_dialog import TagsDialog
-
-__all__ = ['MainWindow', 'AddEditDialog', 'CategoriesDialog', 'TagsDialog']
